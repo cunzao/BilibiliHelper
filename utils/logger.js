780c0f5b3049e901bb75d635ead3d9f09611d613
cunzao <34603326+cunzao@users.noreply.github.com>
2019-05-13 16:12:17 +0800

    BUG FIXED

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,11 @@
 const chalk = require('chalk')
 const datetime = require('silly-datetime')
+var Atime=datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')
 
 module.exports = {
-  debug: message => console.log(`${chalk.bgCyan('DEBUG')} ${datetime} ${message}`),
-  info: message => console.log(`${chalk.bgCyan('INFO')} ${datetime} ${message}`),
-  notice: message => console.log(`${chalk.bgGreen('NOTICE')} ${datetime} ${message}`),
-  warning: message => console.log(`${chalk.bgYellow('WARNING')} ${datetime} ${message}`),
-  error: message => console.log(`${chalk.bgRed('ERROR')} ${datetime} ${message}`),
+  debug: message => console.log(`${chalk.bgCyan('DEBUG')} ${Atime} ${message}`),
+  info: message => console.log(`${chalk.bgCyan('INFO')} ${Atime} ${message}`),
+  notice: message => console.log(`${chalk.bgGreen('NOTICE')} ${Atime} ${message}`),
+  warning: message => console.log(`${chalk.bgYellow('WARNING')} ${Atime} ${message}`),
+  error: message => console.log(`${chalk.bgRed('ERROR')} ${Atime} ${message}`),
 }

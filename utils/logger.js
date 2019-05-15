ae653f173ec4b1a54b36f96a5dde4aaf7e2dcf7d
cunzao <34603326+cunzao@users.noreply.github.com>
2019-05-15 17:51:38 +0800

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,11 @@
 const chalk = require('chalk')
 const datetime = require('silly-datetime')
 datetime.locate('zh-cn')
-var Atime=datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')
 
 module.exports = {
-  debug: message => console.log(`${chalk.bgCyan('DEBUG')} ${Atime} ${message}`),
-  info: message => console.log(`${chalk.bgCyan('INFO')} ${Atime} ${message}`),
-  notice: message => console.log(`${chalk.bgGreen('NOTICE')} ${Atime} ${message}`),
-  warning: message => console.log(`${chalk.bgYellow('WARNING')} ${Atime} ${message}`),
-  error: message => console.log(`${chalk.bgRed('ERROR')} ${Atime} ${message}`),
+  debug: message => console.log(`${chalk.bgCyan('DEBUG')} ${datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')} ${message}`),
+  info: message => console.log(`${chalk.bgCyan('INFO')} ${datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')} ${message}`),
+  notice: message => console.log(`${chalk.bgGreen('NOTICE')} ${datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')} ${message}`),
+  warning: message => console.log(`${chalk.bgYellow('WARNING')} ${datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')} ${message}`),
+  error: message => console.log(`${chalk.bgRed('ERROR')} ${datetime.format(new Date(), 'YYYY-MM-DD HH:mm:ss')} ${message}`),
 }

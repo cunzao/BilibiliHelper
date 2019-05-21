83e498bfb67e7bf02e83d5c8b3b4a7bbb31c8973
cunzao <34603326+cunzao@users.noreply.github.com>
2019-05-21 12:41:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const app = async () => {
     // await module_silver2coin()
     // await module_guard()
     logger.info('Mission Complete!')
-    await sleep(‭82800000‬)
+    await sleep(43200000)
   }
 }
 

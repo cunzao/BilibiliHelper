8de9d2d092496df7a1f1a2f154011810c62928bf
cunzao <34603326+cunzao@users.noreply.github.com>
2019-09-05 11:22:25 +0800

    Update index.js
    恢复除了舰长亲密度和扭蛋机以外的功能

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ const app = async () => {
   while (true) {
     await module_auth()
     await module_tasks()
-    // await module_heart()
-    // await module_silver()
+    await module_heart()
+    await module_silver()
     await module_group()
     // await module_capsule()
-    // await module_giftsend()
+    await module_giftsend()
     await module_dailybag()
     await module_silver2coin()
     // await module_guard()
-    logger.info('Mission Complete!')
-    await sleep(43200000)
+    // await sleep(43200000)
+    await sleep(1000)
   }
 }
 
